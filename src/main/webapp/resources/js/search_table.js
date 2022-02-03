0d2074550136e061a7d07e24766e1189f39a5446
ghd5100 <ghd5100@gmail.com>
2022-02-03 21:13:37 +0900

    220203_hj

--- src/main/webapp/resources/js/search_table.js
+++ src/main/webapp/resources/js/search_table.js
@@ -1,14 +1,16 @@
+/*스트립트 연동되는거 jsext.html예제보긔*/
+
 
 var board = {
     init : function () {
 	   //초기 
 	  //board.getList();
     	console.log("보드 자바스크립트 들어옴");
-    	board.loadContent();
+    	board.makeRow();
      },
  
 
-    loadContent : function(){
+     makeRow : function(){
     	
 		$.ajax({
 	        type: 'POST',
@@ -33,6 +35,23 @@ var board = {
 	    	'<td>'+ 수량/장바구니 + '</td>';
 			'</tr>';
 			
+			
+	        for(k=1;k<=data.documents.length;k++){
+	            //document.write("<h"+i+">Header "+i+"</h"+i+">");
+	            document.write('<tr height="150px">');
+	            document.write('<td>'+ data.documents[k].thumnail + '</td>');
+	            document.write('<p>' + data.documents[k].title + '</p>');
+	            document.write('<p>' + data.documents[k].authors + '</p>');
+	            document.write('<p>' + data.documents[k].publisher + '</p>');
+	            document.write('<p>' + data.documents[k].title + '</p>');
+	            document.write('</td>');
+	            document.write('<td>'+ 평점 + '</td>');
+	            document.write('<td>'+ data.documents[k].price + '</td>');
+	            document.write('<td>'+ 수량/장바구니 + '</td>');
+	            document.write('</tr>');
+	        }
+			
+			
 			$('#getList').done();
 			
 /*	    	for(k in data.documents.length-1; k++){
@@ -48,24 +67,7 @@ var board = {
     	
 	
     	
-/*	        makeRow : function (data){
-    	var html = '';
-    	for(var k=0;k<data.length-1;k++){ 
-    		html += '<tr>' ;  
-    		html +=	'<th scope="row">' + data[k].BID + '</th>';
-    		html += '<td>'+data[k].BNAME+'</td>';
-    		html += '<td>' + '<span style="cursor:pointer" name="contentLink" value='+ data[k].BID +'>'+ data[k].BTITLE + '</span> </td>';
-    		html += '<td>' + data[k].BDATE +  '</td>';
-    		html += '</tr>' ;
-    	}
-    	
-    	html += '<tr>';
-    	html += '<td colspan="5"> <button id="writeButton">글작성</button> </td>';
-    	html += '</tr>';
-    	
-    	$('#tableRows').html(html);
-    }
-    */
+
    }
  
 }
